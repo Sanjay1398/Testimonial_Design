617d2deb39d63bf94427f96c8bbb8e8d63eefb1e
sanjay1398 <sancs1398@gmail.com>
2020-08-12 10:59:51 +0530

    Added multiplyFunc

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
-const addFunc = (a, b) => console.log(a + b);  
+const addFunc = (a, b) => console.log(a + b); 
+const multiplyFunc = (a, b) => console.log(a * b); 
 
 nextBtn.addEventListener('click', changeNextImage);
 prevBtn.addEventListener('click', changePrevImage);
@@ -12,3 +13,4 @@ function changePrevImage() {
   document.getElementById('girl').src = 'images/image-tanya.jpg';
 }
 addFunc(1, 3);
+multiplyFunc(1, 3);
